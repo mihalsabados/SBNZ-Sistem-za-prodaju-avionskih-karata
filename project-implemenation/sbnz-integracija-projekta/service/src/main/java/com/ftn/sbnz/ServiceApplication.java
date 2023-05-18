b1b4b1042086971993f55b40ad67e66c2fed7153
mihalsabados <sabadosmihal00@gmail.com>
2023-05-18 12:38:35 +0200

    Configured ServiceApplication main class

--- project-implemenation/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/ServiceApplication.java
+++ project-implemenation/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/ServiceApplication.java
@@ -24,6 +24,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class ServiceApplication implements CommandLineRunner{
 
+	@Autowired
 	private UserRepository userRepository;
 	
 	private static Logger log = LoggerFactory.getLogger(ServiceApplication.class);
