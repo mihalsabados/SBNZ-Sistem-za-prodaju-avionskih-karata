dd9461e52e1cf21f63e53675af3b31e6d5518219
mihalsabados <sabadosmihal00@gmail.com>
2023-05-28 21:52:36 +0200

    Finished test for cep rules

--- project-implemenation/sbnz-integracija-projekta/service/src/test/java/com/ftn/sbnz/service/tests/CEPSuspiciousTransactionTest.java
+++ project-implemenation/sbnz-integracija-projekta/service/src/test/java/com/ftn/sbnz/service/tests/CEPSuspiciousTransactionTest.java
@@ -17,6 +17,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class CEPSuspiciousTransactionTest {
 
@@ -34,10 +35,9 @@ public class CEPSuspiciousTransactionTest {
         User user = new User("misa@gmail.com", "", "Misa", "Jokic", "Stojanova 22", "0613005551", "Novi Sad", LoyaltyType.BRONZE, UserType.PASSENGER, false);
         Ticket ticket1 = new Ticket(1L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 20:00"));
         Ticket ticket2 = new Ticket(2L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 20:30"));
-        Flight flight1 = new Flight(1L, "New York City", 7978, 100000, ft.parse("27.05.2023 20:00"), new ArrayList<>(Arrays.asList(ticket1, ticket2)), 2, false, "NYC1");
+        Flight flight1 = new Flight(1L, "New York City", 7978, 100000, ft.parse("27.05.2023 20:00"), new ArrayList<>(List.of(ticket2)), 2, false, "NYC1");
 
         ksession.insert(ticket1);
-        ksession.insert(ticket2);
         ksession.insert(flight1);
         ksession.insert(user);
         ksession.setGlobal("flightId", 1L);
@@ -47,7 +47,7 @@ public class CEPSuspiciousTransactionTest {
         Assert.assertEquals(1, rulesFired);
         Assert.assertEquals(1, redundantPaymentEvents.size());
         Assert.assertEquals(0, suspiciousTransactionEvents.size());
-        Assert.assertEquals(false, user.isBlocked());
+        Assert.assertFalse(user.isBlocked());
         ksession.destroy();
     }
 
@@ -60,24 +60,20 @@ public class CEPSuspiciousTransactionTest {
         Ticket ticket1 = new Ticket(1L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 20:00"));
         Ticket ticket2 = new Ticket(2L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 20:30"));
 
-        //second redundant payment event
-        Ticket ticket3 = new Ticket(3L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 20:40"));
-        Ticket ticket4 = new Ticket(4L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 20:50"));
-
-        Flight flight1 = new Flight(1L, "New York City", 7978, 100000, ft.parse("27.05.2023 20:00"), new ArrayList<>(Arrays.asList(ticket1, ticket2, ticket3, ticket4)), 2, false, "NYC1");
+        Flight flight1 = new Flight(1L, "New York City", 7978, 100000, ft.parse("27.05.2023 20:00"), new ArrayList<>(List.of(ticket2)), 2, false, "NYC1");
+        RedundantPaymentEvent rpe = new RedundantPaymentEvent(1L, 1L, ft.parse("28.05.2023 20:00"), user.getEmail(), 2L, 4L);
 
         ksession.insert(ticket1);
-        ksession.insert(ticket2);
-        ksession.insert(ticket3);
-        ksession.insert(ticket4);
         ksession.insert(flight1);
         ksession.insert(user);
+        ksession.insert(rpe);
         ksession.setGlobal("flightId", 1L);
         int rulesFired = ksession.fireAllRules();
         Collection<?> redundantPaymentEvents = ksession.getObjects(new ClassObjectFilter(RedundantPaymentEvent.class));
         Collection<?> suspiciousTransactionEvents = ksession.getObjects(new ClassObjectFilter(SuspiciousTransactionEvent.class));
-        Assert.assertTrue(suspiciousTransactionEvents.size() >= 1);
-        Assert.assertEquals(false, user.isBlocked());
+        Assert.assertEquals(1, suspiciousTransactionEvents.size());
+        Assert.assertEquals(2, redundantPaymentEvents.size());
+        Assert.assertFalse(user.isBlocked());
         ksession.destroy();
     }
 
@@ -90,38 +86,22 @@ public class CEPSuspiciousTransactionTest {
         Ticket ticket1 = new Ticket(1L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 20:00"));
         Ticket ticket2 = new Ticket(2L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 20:30"));
 
-        //second redundant payment event
-        Ticket ticket3 = new Ticket(3L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 20:40"));
-        Ticket ticket4 = new Ticket(4L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 20:50"));
-
-        //third redundant payment event
-        Ticket ticket5 = new Ticket(5L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 21:05"));
-        Ticket ticket6 = new Ticket(6L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 21:10"));
-
-        //fourth redundant payment event
-        Ticket ticket7 = new Ticket(7L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 21:20"));
-        Ticket ticket8 = new Ticket(8L, user, user, null,0, 0, TicketType.ECONOMIC, ft.parse("26.05.2023 21:30"));
-
-        Flight flight1 = new Flight(1L, "New York City", 7978, 100000,
-                ft.parse("27.05.2023 20:00"),
-                new ArrayList<>(Arrays.asList(ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7, ticket8)),
-                2, false, "NYC1");
+        Flight flight1 = new Flight(1L, "New York City", 7978, 100000, ft.parse("27.05.2023 20:00"), new ArrayList<>(List.of(ticket2)), 2, false, "NYC1");
+        RedundantPaymentEvent rpe = new RedundantPaymentEvent(1L, 1L, ft.parse("28.05.2023 20:00"), user.getEmail(), 2L, 4L);
+        SuspiciousTransactionEvent ste = new SuspiciousTransactionEvent(1L, 1L,user.getEmail(), ft.parse("28.05.2023 20:00"), 3L, 4L);
 
         ksession.insert(ticket1);
-        ksession.insert(ticket2);
-        ksession.insert(ticket3);
-        ksession.insert(ticket4);
-        ksession.insert(ticket5);
-        ksession.insert(ticket6);
-        ksession.insert(ticket7);
-        ksession.insert(ticket8);
         ksession.insert(flight1);
         ksession.insert(user);
+        ksession.insert(rpe);
+        ksession.insert(ste);
         ksession.setGlobal("flightId", 1L);
         int rulesFired = ksession.fireAllRules();
         Collection<?> redundantPaymentEvents = ksession.getObjects(new ClassObjectFilter(RedundantPaymentEvent.class));
         Collection<?> suspiciousTransactionEvents = ksession.getObjects(new ClassObjectFilter(SuspiciousTransactionEvent.class));
-        Assert.assertEquals(true, user.isBlocked());
+        Assert.assertEquals(2, suspiciousTransactionEvents.size());
+        Assert.assertEquals(2, redundantPaymentEvents.size());
+        Assert.assertTrue(user.isBlocked());
         ksession.destroy();
     }
 }
